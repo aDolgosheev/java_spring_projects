b01ca2784dd002d836b7134e50aa22f0fe709e10
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-11-07 23:06:45 +0300

    update 2023.11.07

--- spring-app1/src/main/java/ru/dolgosheev/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/dolgosheev/springcourse/TestSpring.java
@@ -12,8 +12,19 @@ public class TestSpring {
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(100);
+        System.out.println(secondMusicPlayer.getVolume());
+
+//        musicPlayer.playMusicList();
 
         context.close();
     }
