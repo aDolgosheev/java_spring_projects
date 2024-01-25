e4fbcf5c2f87c668f1e443ad300a3b145759666d
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-01-25 23:09:57 +0300

    update 2024.01.25

--- HibernateApp_02/src/main/java/ru/dolgosheev/App.java
+++ HibernateApp_02/src/main/java/ru/dolgosheev/App.java
@@ -3,11 +3,12 @@ package ru.dolgosheev;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import ru.dolgosheev.model.Person;
 
-public class App {
-    public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+public class App
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration();
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -15,13 +16,7 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = new Person("Some name", 60);
-            session.persist(person);
-
             session.getTransaction().commit();
-
-            System.out.println(person.getId());
-
         } finally {
             sessionFactory.close();
         }
