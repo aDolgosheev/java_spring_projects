2cff339299dca68de6f6201ce3c66828dd9b746e
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-01-26 15:50:10 +0300

    update 2024.01.26

--- HibernateApp_02/src/main/java/ru/dolgosheev/App.java
+++ HibernateApp_02/src/main/java/ru/dolgosheev/App.java
@@ -3,18 +3,73 @@ package ru.dolgosheev;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.dolgosheev.model.Item;
+import ru.dolgosheev.model.Person;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        Configuration configuration = new Configuration();
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
+
+//            // Получение информации о человеке
+//            Person person = session.get(Person.class, 3);
+//            System.out.println(person);
+//            List<Item> items = person.getItems();
+//            System.out.println(items);
+
+//            // Получение информации о товаре
+//            Item item = session.get(Item.class, 5);
+//            System.out.println(item);
+//            Person person = item.getOwner();
+//            System.out.println(person);
+
+//            // Добавление нового товара человеку
+//            Person person = session.get(Person.class, 2);
+//            Item newItem = new Item("Vacuum Cleaner", person);
+//            person.getItems().add(newItem);
+//            session.persist(newItem);
+
+//            // Создание нового человека и добавление ему нового товара
+//            Person person = new Person("Edward", 35);
+//            Item newItem = new Item("Laptop", person);
+//            person.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+//            session.persist(person);
+//            session.persist(newItem);
+
+//            // Удаление всех товаров у человека
+//            Person person = session.get(Person.class, 3);
+//            List<Item> items = person.getItems();
+//            //SQL
+//            for (Item item : items) {
+//                session.remove(item);
+//            }
+//            //Не порождает SQL, но необходимо для того, чтобы в кэше все было верно
+//            person.getItems().clear();
+
+//            // Удаление человека
+//            Person person = session.get(Person.class, 2);
+//            //SQL
+//            session.remove(person);
+//            //Не порождает SQL, но необходимо для того, чтобы в кэше все было верно
+//            person.getItems().forEach(i -> i.setOwner(null));
+
+            // Изменение владельца товара
+            Person person = session.get(Person.class, 4);
+            Item item = session.get(Item.class, 1);
+            item.getOwner().getItems().remove(item);
+            item.setOwner(person);
+            person.getItems().add(item);
 
             session.getTransaction().commit();
         } finally {
