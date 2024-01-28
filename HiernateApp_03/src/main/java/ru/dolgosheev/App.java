692a2f6595283d0e19308cc181c8b4b7be81204c
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-01-28 22:22:34 +0300

    update 2024.01.28

--- HiernateApp_03/src/main/java/ru/dolgosheev/App.java
+++ HiernateApp_03/src/main/java/ru/dolgosheev/App.java
@@ -3,25 +3,34 @@ package ru.dolgosheev;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.dolgosheev.model.Actor;
+import ru.dolgosheev.model.Movie;
 import ru.dolgosheev.model.Passport;
 import ru.dolgosheev.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
     public static void main( String[] args )
     {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+//                .addAnnotatedClass(Person.class)
+//                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+
+        // try with resources
+        try(sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            session.remove(person);
+//            Person person = session.get(Person.class, 1);
+//            session.remove(person);
 
 //            person.getPassport().setPassportNumber(555777);
 //            System.out.println(person.getPassport().getPassportNumber());
@@ -31,9 +40,13 @@ public class App
 
 //            session.persist(person);
 
+            Movie movie = new Movie("Pulp function", 1994);
+            Actor actor1 = new Actor("Harvey Keitel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+
+            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+
             session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
         }
     }
 }
