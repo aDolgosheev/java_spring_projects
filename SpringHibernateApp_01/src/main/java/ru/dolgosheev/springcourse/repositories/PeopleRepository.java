b95e6b53e7a133fedc628dafb10fc52789a87043
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-02-04 22:48:42 +0300

    update 2024.02.04

---
+++ SpringHibernateApp_01/src/main/java/ru/dolgosheev/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package ru.dolgosheev.springcourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.dolgosheev.springcourse.models.Person;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+
+
+}
