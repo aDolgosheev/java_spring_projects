ec8c70f8682c786731d95d9c123e8daba8d7bc0c
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-02-15 15:49:08 +0300

    update 2024.02.15

--- SpringHibernateApp_01/src/main/java/ru/dolgosheev/springcourse/controllers/PeopleController.java
+++ SpringHibernateApp_01/src/main/java/ru/dolgosheev/springcourse/controllers/PeopleController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.dolgosheev.springcourse.models.Person;
+import ru.dolgosheev.springcourse.services.ItemService;
 import ru.dolgosheev.springcourse.services.PeopleService;
 
 import javax.validation.Valid;
@@ -16,14 +17,23 @@ public class PeopleController {
 
     private final PeopleService peopleService;
 
+    private final ItemService itemService;
+
     @Autowired
-    public PeopleController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService, ItemService itemService) {
         this.peopleService = peopleService;
+        this.itemService = itemService;
     }
 
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", peopleService.findAll());
+
+        itemService.findByItemName("Airpods");
+        itemService.findByOwner(peopleService.findAll().get(0));
+
+        peopleService.test();
+
         return "people/index";
     }
 
--- SpringHibernateApp_01/src/main/java/ru/dolgosheev/springcourse/repositories/ItemsRepository.java
+++ SpringHibernateApp_01/src/main/java/ru/dolgosheev/springcourse/repositories/ItemsRepository.java
@@ -1,5 +1,6 @@
 package ru.dolgosheev.springcourse.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.dolgosheev.springcourse.models.Item;
 import ru.dolgosheev.springcourse.models.Person;
@@ -7,7 +8,7 @@ import ru.dolgosheev.springcourse.models.Person;
 import java.util.List;
 
 @Repository
-public interface ItemsRepository {
+public interface ItemsRepository extends JpaRepository<Item, Integer> {
 
     List<Item> findByItemName(String itemName);
 
